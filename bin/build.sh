43515f2e1e2f634dcb81a6066944d6152b37e76c
branles14 <135960981+branles14@users.noreply.github.com>
2023-06-18 20:36:35 -0600

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 build_example() {
-    local msg_1
-    local msg_2
-    local msg_3
-    local msg_4
+    local msg_1=""
+    local msg_2=""
+    local msg_3=""
+    local msg_4=""
 
     while [[ -z $msg_1 || -z $msg_2 || -z $msg_3 || -z $msg_4 ]]; do
         msg_1=$(shuf -n 1 data/1.txt)
@@ -13,23 +13,29 @@ build_example() {
         msg_4=$(shuf -n 1 data/4.txt)
     done
 
-    echo "<|first|>$msg_1<|second|>$msg_2<|third|>$msg_3<|forth|>$msg_4<|endoftext|>"
+    echo "<first>$msg_1<second>$msg_2<third>$msg_3<forth>$msg_4<end>"
 }
 
 main() {
     # Error checks
-    for i in {1..4}; do
-        if [[ ! -f "data/$i.txt" || $(wc -l < "data/$i.txt") -lt 500 ]]; then
-            echo "Error: 'data/$i.txt' either doesn't exist or contains less than 500 lines."
-            exit 1
-        fi
+    if [[ ! -f "data/1.txt" || ! -f "data/2.txt" || ! -f "data/3.txt" || ! -f "data/4.txt" ]]; then
+        echo "Error: One or more data files are missing."
+        exit 1
+    fi
+
+    if [[ $(wc -l < data/1.txt) -lt 500 || $(wc -l < data/2.txt) -lt 500 || $(wc -l < data/3.txt) -lt 500 || $(wc -l < data/4.txt) -lt 500 ]]; then
+        echo "Error: One or more data files have less than 500 lines."
+        exit 1
+    fi
+
+    # Generate examples and output to data/train.txt
+    while [[ $(wc -l < data/train.txt) -lt 100000 ]]; do
+        build_example >> data/train.txt
+        sort -u -o data/train.txt data/train.txt
     done
 
-    # Generate 'data/train.txt'
-    while [[ $(wc -l < "data/train.txt") -lt 10000 ]]; do
-        build_example >> "data/train.txt"
-        sort -u -o "data/train.txt" "data/train.txt"
-    done
+    # Shuffle lines in data/train.txt
+    shuf -o data/train.txt data/train.txt
 }
 
 main
